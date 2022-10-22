3ee1435a991bf5fcaa197c60c893cdceb6d53e32
Chris Gregg <tofergregg@gmail.com>
2022-10-22 10:06:48 -0700

    testing

--- js/webworker.js
+++ js/webworker.js
@@ -6,26 +6,27 @@
 importScripts("https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js");
 
 async function loadPyodideAndPackages() {
-  self.pyodide = await loadPyodide();
-  await self.pyodide.loadPackage(["numpy", "pytz"]);
+    self.pyodide = await loadPyodide();
+    await self.pyodide.loadPackage(["numpy", "pytz"]);
 }
 let pyodideReadyPromise = loadPyodideAndPackages();
 
 self.onmessage = async (event) => {
-  // make sure loading is done
-  await pyodideReadyPromise;
-  // Don't bother yet with this line, suppose our API is built in such a way:
-  const { id, python, ...context } = event.data;
-  // The worker copies the context in its own "memory" (an object mapping name to values)
-  for (const key of Object.keys(context)) {
-    self[key] = context[key];
-  }
-  // Now is the easy part, the one that is similar to working in the main thread:
-  try {
-    await self.pyodide.loadPackagesFromImports(python);
-    let results = await self.pyodide.runPythonAsync(python);
-    self.postMessage({ results, id });
-  } catch (error) {
-    self.postMessage({ error: error.message, id });
-  }
+    // make sure loading is done
+    await pyodideReadyPromise;
+    // Don't bother yet with this line, suppose our API is built in such a way:
+    const { id, python, ...context } = event.data;
+    // The worker copies the context in its own "memory" (an object mapping name to values)
+    for (const key of Object.keys(context)) {
+        self[key] = context[key];
+    }
+    // Now is the easy part, the one that is similar to working in the main thread:
+    try {
+        console.log(python);
+        await self.pyodide.loadPackagesFromImports(python);
+        let results = await self.pyodide.runPythonAsync(python);
+        self.postMessage({ results, id });
+    } catch (error) {
+        self.postMessage({ error: error.message, id });
+    }
 };
