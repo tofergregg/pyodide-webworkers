e6c7347c7d79c0db4b8cf8a4744d1c8ffd559ae6
Chris Gregg <tofergregg@gmail.com>
2022-10-22 11:39:34 -0700

    testing input

--- js/py-worker.js
+++ js/py-worker.js
@@ -3,10 +3,12 @@ const pyodideWorker = new Worker("./js/webworker.js");
 const callbacks = {};
 
 pyodideWorker.onmessage = (event) => {
-  const { id, ...data } = event.data;
-  const onSuccess = callbacks[id];
-  delete callbacks[id];
-  onSuccess(data);
+    const { id, ...data } = event.data;
+    const onSuccess = callbacks[id];
+    console.log("onSuccess:");
+    console.log(onSuccess);
+    delete callbacks[id];
+    onSuccess(data);
 };
 
 const sendMessageToWorker = (message) => {
@@ -15,19 +17,19 @@ const sendMessageToWorker = (message) => {
 };
 
 const asyncRun = (() => {
-  let id = 0; // identify a Promise
-  return (script, context) => {
-    // the id could be generated more carefully
-    id = (id + 1) % Number.MAX_SAFE_INTEGER;
-    return new Promise((onSuccess) => {
-      callbacks[id] = onSuccess;
-      pyodideWorker.postMessage({
-        ...context,
-        python: script,
-        id,
-      });
-    });
-  };
+    let id = 0; // identify a Promise
+    return (script, context) => {
+        // the id could be generated more carefully
+        id = (id + 1) % Number.MAX_SAFE_INTEGER;
+        return new Promise((onSuccess) => {
+            callbacks[id] = onSuccess;
+            pyodideWorker.postMessage({
+                ...context,
+                python: script,
+                id,
+            });
+        });
+    };
 })();
 
 export { asyncRun, sendMessageToWorker };
