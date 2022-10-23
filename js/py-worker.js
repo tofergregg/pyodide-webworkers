1a0835bdb65c9e2a5efb4f27e2b231cc9eaf77c3
Chris Gregg <tofergregg@gmail.com>
2022-10-23 14:12:22 -0700

    user input

--- js/py-worker.js
+++ js/py-worker.js
@@ -71,7 +71,7 @@ const getInputFromTerminal = () => {
         if (!currentVal.startsWith(originalText)) {
             terminal.value = originalText + userInput;
         } else {
-            userInput = currentText.substring(originalText.length);
+            userInput = currentVal.substring(originalText.length);
             console.log("User input so far: " + userInput);
         }
   }, false);
