f664f0a63da2ebeae5435203a8259b4d91e33429
Chris Gregg <tofergregg@gmail.com>
2022-10-22 19:49:08 -0700

    trying to get change to be recognized

--- js/webworker.js
+++ js/webworker.js
@@ -81,10 +81,10 @@ function sleep_fixed(t) {
 }
 
 function wait_for_js_message() {
-    // waiting_for_message();
     // console.log(self.sharedBuf[0]);
     if (self.sharedBuf[0] != 0) {
-        console.log(sharedBuf[0]);
+        // console.log(sharedBuf[0]);
+        waiting_for_message();
         return self.sharedBuf[0];
     } else {
         return null;
