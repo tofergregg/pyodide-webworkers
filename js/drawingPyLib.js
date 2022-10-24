9776d3f77f477955e6920ae7a50f7fccb2d6299b
Chris Gregg <tofergregg@gmail.com>
2022-10-23 22:40:02 -0700

    added drawingPyLib.js

---
+++ js/drawingPyLib.js
@@ -0,0 +1,20 @@
+const drawingLib = `from js import passDrawShape
+
+def draw_line(x1, y1, x2, y2, color='black'):
+    passDrawShape(['line', x1, y1, x2, y2, color]) 
+
+def draw_rect(x, y, width, height, color='black'):
+    passDrawShape(['rect', x, y, width, height, color]) 
+
+def fill_rect(x, y, width, height, color='black'):
+    passDrawShape(['rect', x, y, width, height, color, False, True]) 
+
+def draw_oval(x, y, width, height, color='black'):
+    passDrawShape(['oval', x, y, width, height, color]) 
+
+def fill_oval(x, y, width, height, color='black'):
+    passDrawShape(['oval', x, y, width, height, color, False, True]) 
+
+def draw_string(x, y, text, color='black'):
+    passDrawShape(['text', x, y, text, color]) 
+`
--- js/webworker.js
+++ js/webworker.js
@@ -4,27 +4,7 @@
 // `pyodide.js`, and all its associated `.asm.js`, `.data`, `.json`,
 // and `.wasm` files as well:
 importScripts("https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js");
-
-const drawingLib = `from js import passDrawShape
-
-def draw_line(x1, y1, x2, y2, color='black'):
-    passDrawShape(['line', x1, y1, x2, y2, color]) 
-
-def draw_rect(x, y, width, height, color='black'):
-    passDrawShape(['rect', x, y, width, height, color]) 
-
-def fill_rect(x, y, width, height, color='black'):
-    passDrawShape(['rect', x, y, width, height, color, False, True]) 
-
-def draw_oval(x, y, width, height, color='black'):
-    passDrawShape(['oval', x, y, width, height, color]) 
-
-def fill_oval(x, y, width, height, color='black'):
-    passDrawShape(['oval', x, y, width, height, color, False, True]) 
-
-def draw_string(x, y, text, color='black'):
-    passDrawShape(['text', x, y, text, color]) 
-`
+importScripts("./drawingPyLib.js");
 
 async function loadPyodideAndPackages() {
     let first = true;
