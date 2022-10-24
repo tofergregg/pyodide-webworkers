da7d0783b76865d04755fd2751ab94cd016f1bbc
Chris Gregg <tofergregg@gmail.com>
2022-10-23 22:19:00 -0700

    testing DrawShape

--- js/webworker.js
+++ js/webworker.js
@@ -7,23 +7,23 @@ importScripts("https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js");
 
 const drawingLib = `from js import passDrawShape
 
-def draw_line(x1, y1, x2, y2, color='black'):
+def draw_line([x1, y1, x2, y2, color='black']):
     passDrawShape('line', x1, y1, x2, y2, color) 
 
-def draw_rect(x, y, width, height, color='black'):
+def draw_rect([x, y, width, height, color='black']):
     passDrawShape('rect', x, y, width, height, color) 
 
-def fill_rect(x, y, width, height, color='black'):
-    passDrawShape('rect', x, y, width, height, color, False, True) 
+def fill_rect([x, y, width, height, color='black']):
+    passDrawShape(['rect', x, y, width, height, color, False, True]) 
 
 def draw_oval(x, y, width, height, color='black'):
-    passDrawShape('oval', x, y, width, height, color) 
+    passDrawShape(['oval', x, y, width, height, color]) 
 
 def fill_oval(x, y, width, height, color='black'):
-    passDrawShape('oval', x, y, width, height, color, False, True) 
+    passDrawShape(['oval', x, y, width, height, color, False, True]) 
 
 def draw_string(x, y, text, color='black'):
-    passDrawShape('text', x, y, text, color) 
+    passDrawShape(['text', x, y, text, color]) 
 `
 
 async function loadPyodideAndPackages() {
