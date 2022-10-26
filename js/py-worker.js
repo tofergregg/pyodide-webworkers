994d3774f33bf36aa8cfb0652935670e2e7c68f5
Chris Gregg <tofergregg@gmail.com>
2022-10-25 20:18:04 -0700

    testing mouse

--- js/py-worker.js
+++ js/py-worker.js
@@ -57,6 +57,7 @@ const setupWorker = () => {
             const lastX = window.lastMouse.x;
             const lastY = window.lastMouse.y;
 
+            console.log(window.lastMouse.x);
             Atomics.store(window.sharedArr, 1, window.lastMouse.x % 256);
             Atomics.store(window.sharedArr, 2, Math.floor(window.lastMouse.x / 256));
 
