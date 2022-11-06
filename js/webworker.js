d05281c24e9488267725257334055670127f3489
Chris Gregg <tofergregg@gmail.com>
2022-11-06 13:32:36 -0800

    fixed type in Atomics.wait

--- js/webworker.js
+++ js/webworker.js
@@ -74,7 +74,8 @@ function input_fixed(text) {
     // console.log("input requested: " + text)
     self.postMessage({outputText: text, getInput: true});
     // while (Atomics.load(self.sharedBuf, 0) == 0) {} // spin
-    Atomics.wait(self.sharedBuf, 0, 0);
+    let sharedArr = Uint8Array(self.sharedBuf);
+    Atomics.wait(sharedArr, 0, 0);
     // reset trigger
     Atomics.store(self.sharedBuf, 0, 0);
 
@@ -114,7 +115,8 @@ function getMousePos(x_or_y) {
     Atomics.store(self.sharedBuf, 0, 0);
     self.postMessage({cmd: 'getMousePos'});
     // while (Atomics.load(self.sharedBuf, 0) == 0) {}; // spin
-    Atomics.wait(self.sharedBuf, 0, 0);
+    let sharedArr = Uint8Array(self.sharedBuf);
+    Atomics.wait(sharedArr, 0, 0);
     if (x_or_y == 'x') {
         return Atomics.load(self.sharedBuf, 1) + Atomics.load(self.sharedBuf, 2) * 256;
     } else {
