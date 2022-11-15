a79d7732c4334eb4cd32eceaeed1e1e6410a60d0
Chris Gregg <tofergregg@gmail.com>
2022-11-14 22:32:55 -0800

    needed selfs

--- js/drawingLib2.py
+++ js/drawingLib2.py
@@ -2,18 +2,18 @@ class Canvas:
     def __init__(self, width=285, height=244):
         self.width = width 
         self.height = height
-        objects = []
+        self.objects = []
 
     def move(obj, x_amount, y_amount):
-        objects[obj]['coords'][0] += x_amount
-        objects[obj]['coords'][1] += y_amount
-        objects[obj]['coords'][2] += x_amount
-        objects[obj]['coords'][3] += y_amount
+        self.objects[obj]['coords'][0] += x_amount
+        self.objects[obj]['coords'][1] += y_amount
+        self.objects[obj]['coords'][2] += x_amount
+        self.objects[obj]['coords'][3] += y_amount
         updateCanvas(['move', {'obj': obj, 'x_amount': x_amount, 'y_amount': y_amount}])
-        return objects[obj]['coords']
+        return self.objects[obj]['coords']
 
     def delete(obj):
-        objects.pop(obj)
+        self.objects.pop(obj)
         updateCanvas(['delete', {'obj': obj}])
 
     def coords(obj, x1=None, y1=None, x2=None, y2=None):
@@ -23,10 +23,10 @@ class Canvas:
         """
         if x1 is None:
             # all four must be present
-            return objects[obj]['coords']
-        objects[obj]['coords'] = [x1, y1, x2, y2]
+            return self.objects[obj]['coords']
+        self.objects[obj]['coords'] = [x1, y1, x2, y2]
         updateCanvas(['coords', {'obj': obj, 'coords': [x1, y1, x2, y2]}])
-        return objects[obj]['coords']
+        return self.objects[obj]['coords']
 
     def create_line(self, x1, y1, x2, y2, color='black'):
         self.objects.append({'obj': 'line', 'coords': [x1, y1, x2, y2], 'color': color, 'fill': ''})
