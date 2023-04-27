5d15f8a29276619de573b48a32521b2be880fa59
Chris Gregg <tofergregg@gmail.com>
2023-04-27 10:31:48 -0700

    working on input

--- js/webworker.js
+++ js/webworker.js
@@ -73,17 +73,9 @@ self.onmessage = async (event) => {
 
 function input_fixed(text) {
     console.log("input requested: " + text)
-    Atomics.store(self.waitArr, 0, 0);
     self.postMessage({outputText: text, getInput: true});
-    // while (Atomics.load(self.waitArr, 0) == 0) {} // spin
-    Atomics.wait(self.waitArr, 0, 0);
 
-    const dataLen = Atomics.load(self.sharedArr, 1) + Atomics.load(self.sharedArr, 2) * 256;
-    let data = '';
-    for (let i = 0; i < dataLen; i++) {
-        data += String.fromCharCode(Atomics.load(self.sharedArr, i + 3));
-    }
-
+    data = "4"
     return data;
 };
 
