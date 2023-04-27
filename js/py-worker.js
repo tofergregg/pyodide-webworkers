7dd9dcacb5855187e58012fcbe02a5347e7df4c0
Chris Gregg <tofergregg@gmail.com>
2023-04-27 15:39:51 -0700

    input with microtask

--- js/py-worker.js
+++ js/py-worker.js
@@ -110,7 +110,9 @@ const setupWorker = () => {
 const sendMessageToWorker = (message) => {
     // console.log("Sending ");
     // console.log({'control': true, "message": message}); 
-    pyodideWorker.postMessage({'control': true, "message": message});
+    queueMicrotask(() => {
+        pyodideWorker.postMessage({'control': true, "message": message});
+    });
 };
 
 const passSharedBuffer = (buf, waitBuf) => {
