7beae2a2948f05e9e69a017c00ff2b25bb446db0
Chris Gregg <tofergregg@gmail.com>
2023-04-27 22:25:33 -0700

    global var

--- js/site.js
+++ js/site.js
@@ -114,7 +114,7 @@ async function transform_code_for_async(code) {
     pyodide.globals.set('the_code', code);
     const transform_code = `import ast
 
-___parse_functions = ['input']
+global ___parse_functions = ['input']
 class TransformFunc(ast.NodeTransformer):
     global ___parse_functions
     def visit_FunctionDef(self, node):
