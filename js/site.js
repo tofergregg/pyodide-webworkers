23591df8ab0978dbec5811b94e35e11f5644ab94
Chris Gregg <tofergregg@gmail.com>
2023-04-29 00:43:35 -0700

    trying to pre-load pyodide

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ import { setupWorker,
 } from "./py-worker.js";
 
 const init_main = () => {
-    window.pyodide = await loadPyodide();
+    window.pyodide = loadPyodide();
     window.stopExecution = false; // we aren't running
     const currentValue = window.cmEditor.state.doc.toString();
     const endPosition = currentValue.length;
