9e56f68535c217a1c7fc9a5444454527d5508d45
Chris Gregg <tofergregg@gmail.com>
2023-04-30 15:50:24 -0700

    fixed time

--- js/webworker.js
+++ js/webworker.js
@@ -149,7 +149,7 @@ function sleep_fixed(t) {
     if (t < 0.004) { // minimum sleep 
         return;
     }
-    settimeout({
+    settimeout(function(){
         sleep_fixed(t - 0.004);
     }, 0.004);
 
