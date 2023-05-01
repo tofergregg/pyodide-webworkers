220ba2cf8e64c05ad862be91f0268a646966a197
Chris Gregg <tofergregg@gmail.com>
2023-04-30 17:54:01 -0700

    testing onmessage

--- js/py-worker.js
+++ js/py-worker.js
@@ -35,6 +35,7 @@ const setupWorker = () => {
     window.pyodideWorker = pyodideWorker;
 
     pyodideWorker.onmessage = (event) => {
+        console.log(event);
         const { id, ...data } = event.data;
         if (event.data.outputText !== undefined) {
             // console.log(event.data.outputText);
