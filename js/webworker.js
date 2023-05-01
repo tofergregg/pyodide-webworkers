862de3cb03abbd1a74ebd1277744910cddbfefbd
Chris Gregg <tofergregg@gmail.com>
2023-04-30 18:21:51 -0700

    testing mouse

--- js/webworker.js
+++ js/webworker.js
@@ -140,8 +140,7 @@ function updateCanvas(cmd, dict) {
 const waitForMousePos = (r) => {
     if (getMousePos.x !== null && getMousePos.y !== null) {
         console.log("returning " + getMousePos.x + ", " + getMousePos.y);
-        return 1234;
-        // return pyodide.toPy({'done': true, 'x': getMousePos.x, 'y': getMousePos.y})
+        return r(pyodide.toPy({'done': true, 'x': getMousePos.x, 'y': getMousePos.y}));
     }
     setTimeout(() => {
         waitForMousePos(r);
