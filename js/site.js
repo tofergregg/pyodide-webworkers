0ed3f763c36787d016ff5834d5b3b64065c70030
Chris Gregg <tofergregg@gmail.com>
2023-05-15 16:29:27 -0700

    testing c4

--- js/site.js
+++ js/site.js
@@ -693,6 +693,7 @@ def copy_board(board):
 
 def ai_turn(board, color, other_color):
     print("in ai_turn")
+    drop_piece(board, col, color)
     return 0
     # go in a winning space if we can
     col = col_to_win(board, color)
