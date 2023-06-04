a53af45864fd6917c3396366ccb89b2c0c8fe036
Chris Gregg <tofergregg@gmail.com>
2023-06-04 12:01:11 -0700

    again...

--- js/site.js
+++ js/site.js
@@ -34,13 +34,13 @@ const init_main = () => {
     canvas._objects = [];
     // set up for mouse movement
     window.lastMouse = {x: 0, y: 0};
-    canvas.addEventListener("mousemove", mouseMove, {capture: false})
-    canvas.addEventListener("touchmove", mouseMove, {capture: false});
+    canvas.addEventListener("mousemove", mouseMove, {passive: false})
+    canvas.addEventListener("touchmove", mouseMove, {passive: false});
 
     // set up for mouse button
     window.lastMouseDown = {x: 0, y: 0};
-    canvas.addEventListener("mousedown", mouseDown, {capture: false})
-    canvas.addEventListener("touchstart", mouseDown,{capture: false})
+    canvas.addEventListener("mousedown", mouseDown, {passive: false})
+    canvas.addEventListener("touchstart", mouseDown,{passive: false})
 
     setupWorker();
 }
