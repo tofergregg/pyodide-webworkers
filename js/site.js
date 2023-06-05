ace865bdfe31d6c87e549d61e0e80094d51c290c
Chris Gregg <tofergregg@gmail.com>
2023-06-04 20:01:11 -0700

    better ball demo

--- js/site.js
+++ js/site.js
@@ -311,7 +311,8 @@ def main():
                 ball['dx'] *= -1
             if ball['y'] > canvas.height - radius or ball['y'] < 0:
                 ball['dy'] *= -1
-        print(f"first ball: x: {balls[0]['x']}, y: {balls[0]['y']}")
+        print(f"first ball: x: {round(balls[0]['x'], 2)}, 
+                            y: {round(balls[0]['y'], 2)}")
         time.sleep(0.01)
         clear_terminal()
 
